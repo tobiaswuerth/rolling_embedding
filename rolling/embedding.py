d379402de27526f1a21a69b2e53811289b30564e
tobiaswuerth <21124705+tobiaswuerth@users.noreply.github.com>
2025-04-07 12:48:10 +0200

    adjust paper data structure, introduce different embedding generation strategy with local modal that supports batching

--- rolling/embedding.py
+++ rolling/embedding.py
@@ -1,12 +1,54 @@
-import ollama
-import numpy as np
+import torch
+from transformers import AutoModelForTokenClassification, AutoTokenizer
 from scipy.spatial.distance import cosine
-
-
-def get_embedding(text):
-    emb = ollama.embeddings(model="nomic-embed-text", prompt=text)
-    return np.array(emb.embedding, dtype=np.float64)
 
 
 def get_similarity(embedding1, embedding2):
     return 1 - cosine(embedding1, embedding2)
+
+
+class GTEEmbeddingModel(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        self.device = "cuda" if torch.cuda.is_available() else "cpu"
+        model_name = "Alibaba-NLP/gte-multilingual-base"
+        self.tokenizer = AutoTokenizer.from_pretrained(model_name)
+        self.model: torch.nn.Module = AutoModelForTokenClassification.from_pretrained(
+            model_name, trust_remote_code=True, torch_dtype=torch.float16
+        )
+        self.model.to(self.device).eval()
+
+        self.vocab_size = self.model.config.vocab_size
+        self.max_size = self.model.config.max_position_embeddings
+
+    @torch.no_grad()
+    def encode(self, texts: list[str], batch_size: int = 64):
+        if isinstance(texts, str):
+            texts = [texts]
+
+        num_texts = len(texts)
+        embeddings = []
+        for n, i in enumerate(range(0, num_texts, batch_size)):
+            batch = texts[i : i + batch_size]
+            embeddings.append(self._encode(batch))
+
+        embeddings = torch.cat(embeddings, dim=0)
+        embeddings = embeddings.cpu().numpy()
+        return embeddings
+
+    @torch.no_grad()
+    def _encode(self, texts: list[str]):
+        text_input = self.tokenizer(
+            texts,
+            padding=True,
+            truncation=True,
+            return_tensors="pt",
+            max_length=self.max_size,
+        )
+        text_input = {k: v.to(self.device) for k, v in text_input.items()}
+        model_out = self.model(**text_input, return_dict=True)
+
+        dense_vecs = model_out.last_hidden_state[:, 0]
+        dense_vecs = torch.nn.functional.normalize(dense_vecs, dim=-1)
+        return dense_vecs
--- rolling/paper.py
+++ rolling/paper.py
@@ -1,130 +1,93 @@
 import regex as re
 import pickle
-# import nltk
-# from nltk.tokenize import sent_tokenize
+import numpy as np
 from dataclasses import dataclass
-
-from .embedding import get_embedding
 
 
 @dataclass
 class Paper:
     title: str
     text: str
-    nodes: list['PaperNode']
-
-    @property
-    def embeddings(self) -> list[float]:
-        def _get_embeddings(node: PaperNode) -> list[float]:
-            embs = [node.embedding]
-            if node.children:
-                for child in node.children:
-                    embs.extend(_get_embeddings(child))
-            return embs
-
-        embs = []
-        for node in self.nodes:
-            embs.extend(_get_embeddings(node))
-        return embs
-    
-    @property
-    def texts(self) -> str:
-        def _get_texts(node: PaperNode) -> str:
-            texts = [node.text]
-            if node.children:
-                for child in node.children:
-                    texts.extend(_get_texts(child))
-            return texts
-
-        txts = []
-        for node in self.nodes:
-            txts.extend(_get_texts(node))
-        return txts
-    
-
-@dataclass
-class PaperNode:
-    text: str
     embedding: list[float]
-    children: list['PaperNode'] = None
+    text_segment_slices: list[tuple[int, int]]
+    text_segment_embeddings: list[list[float]]
 
-    
-def create_paper(title:str, text:str, min_sentence_length=256, max_sentence_length=2048) -> Paper:
-    title = re.sub(r'\s+', ' ', title).strip()
-    text = re.sub(r'\s+', ' ', text).strip()
-    # nltk.download('punkt', quiet=True, raise_on_error=True)
-    # nltk.download('punkt_tab', quiet=True, raise_on_error=True)
-    # sentences = sent_tokenize(text)
-    sentences = text.split(' ')
-
-    # combine short sentences
-    valid_sentences = []
-    current_sentence = ''
-    for sentence in sentences:
-        current_sentence += ' ' + sentence.strip()
-        if len(current_sentence.strip()) >= min_sentence_length:
-            valid_sentences.append(current_sentence.strip())
-            current_sentence = ''
-    if current_sentence:
-        valid_sentences[-1] += ' ' + current_sentence.strip()
-    
-    # generate base nodes
-    all_nodes = []
-    for sent in valid_sentences:
-        emb = get_embedding(sent)
-        all_nodes.append(PaperNode(text=sent, embedding=emb))
-
-    # generate tree layer
-    layer_groups = []
-    current_group = []
-    for node in all_nodes:
-        group_txt_length = sum(len(n.text) for n in current_group)
-
-        if group_txt_length + len(node.text) > max_sentence_length:
-            if len(current_group) > 0:
-                layer_groups.append(current_group)
-            current_group = [node]
-            continue
-        
-        current_group.append(node)
-    
-    if len(current_group) > 0:
-        layer_groups.append(current_group)
-
-    root_nodes = []
-    for group in layer_groups:
-        if len(group) == 1:
-            root_nodes.append(group[0])
-            continue
-
-        group_text = ' '.join([node.text for node in group])
-        group_emb = get_embedding(group_text)
-        root_node = PaperNode(text=group_text, embedding=group_emb, children=group)
-        root_nodes.append(root_node)
-    
-    assert len(root_nodes) > 0, 'No root nodes found.'
-    return Paper(title, text, root_nodes)
+    @property
+    def text_segments(self) -> list[str]:
+        return [self.text[s[0] : s[1]] for s in self.text_segment_slices]
 
 
-def print_paper(paper: Paper, indent: int = 2):
-    print(f'Paper: {paper.title}')
+def create_paper(
+    title: str,
+    text: str,
+    embedding_function: callable,
+    segment_lengths: list[int] = (512, 1024, 2048, 4096),
+) -> Paper:
 
-    def print_node(node: PaperNode, level: int = 0):
-        print(' ' * indent * level + f'> {node.embedding[:3]} {node.text}')
-        if node.children:
-            for child in node.children:
-                print_node(child, level + 1)
+    title = re.sub(r"\s+", " ", title).strip()
+    text = re.sub(r"\s+", " ", text).strip()
 
-    for node in paper.nodes:
-        print_node(node, 1)
+    max_idx = len(text)
+
+    segment_slices = []
+    for segment_length in segment_lengths:
+        segment_count = max_idx // segment_length
+
+        if segment_count < 2:
+            # if the text is too short, just return the whole text as one segment and break
+            segment_slices.append((0, max_idx))
+            break
+
+        actual_segment_length = max_idx / segment_count
+        step_size = actual_segment_length / 2
+        segment_count = segment_count + (segment_count - 1)  # assume 50% overlap
+
+        indecies = []
+        for si in range(segment_count):
+            start = si * step_size
+            end = min(start + actual_segment_length, max_idx)
+
+            start = (int)(np.round(start, 0).astype(np.uint32))
+            end = (int)(np.round(end, 0).astype(np.uint32))
+            indecies.append((start, end))
+        indecies[-1] = (indecies[-1][0], max_idx)  # assure last segment is full length
+
+        # adjust indecies to not cut words
+        for ifrom, ito in indecies:
+            if ifrom > 0 and text[ifrom] != " ":
+                while ifrom > 0 and text[ifrom] != " ":
+                    ifrom -= 1
+            if ito < max_idx and text[ito] != " ":
+                while ito < max_idx and text[ito] != " ":
+                    ito += 1
+            segment_slices.append((ifrom, ito))
+
+    segment_slices = np.array(segment_slices, dtype=np.uint32)
+    texts = [text[s[0] : s[1]] for s in segment_slices]
+    segment_embeddings = embedding_function(texts)
+    embedding = np.mean(segment_embeddings, axis=0)
+
+    return Paper(
+        title=title,
+        text=text,
+        embedding=embedding,
+        text_segment_slices=segment_slices,
+        text_segment_embeddings=segment_embeddings,
+    )
+
+
+def print_paper(paper: Paper):
+    print(f"Paper: {paper.embedding[:5]} {paper.title}")
+    for i, (s, e) in enumerate(paper.text_segment_slices):
+        print(f" > {i:03d} {paper.text_segment_embeddings[i][:5]} {paper.text[s:e]}")
 
 
 def save_paper(paper: Paper, path: str):
-    with open(path, 'wb') as f:
+    with open(path, "wb") as f:
         pickle.dump(paper, f)
 
 
 def load_paper(path: str) -> Paper:
-    with open(path, 'rb') as f:
+    with open(path, "rb") as f:
         paper = pickle.load(f)
     return paper
